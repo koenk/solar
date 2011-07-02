fd651676a96ab1a97520b6d82ea539128398fccc
Koen Koning <koenkoning@gmail.com>
2011-07-02 01:28:09 +0000

    Solar: resol script: - now supports checksums and septet bytes. - parses the real payload. - retries on other-client-connected, not-enough-data fail.

--- resol.py
+++ resol.py
@@ -21,7 +21,7 @@ def login():
     print
     print
     
-    parsebytes(recv())
+    while parsestream(recv()): pass
     
 def recv():
     print "Receiving..."
@@ -35,7 +35,10 @@ def send(dat):
     print "> '%s' (%s)" % (dat, ' '.join([str(ord(i)) for i in dat]))
     sock.send(dat)
     
-def parsebytes(data):
+def parsestream(data):
+    if data.count(chr(0xAA)) < 2:
+        return True
+
     msgs = data.split(chr(0xAA))[1:-1]
     for msg in msgs:
         print ' '.join([hex(ord(i))[2:] for i in msg])
@@ -46,8 +49,8 @@ def parsebytes(data):
         command =   gb(msg,5,7)
         print "Target:\t\t",    hex(target)
         print "Source:\t\t",    hex(source)
-        print "Protocol:\t",  hex(protocol)
-        print "Command:\t",   hex(command)
+        print "Protocol:\t",    hex(protocol)
+        print "Command:\t",     hex(command)
         
         if protocol == 0x10:
             print "PROT 1: ",
@@ -55,7 +58,13 @@ def parsebytes(data):
                 print "DATA"
                 
                 frames = gb(msg,7,8)
-                print "Data frames:\t", hex(frames)
+                chk =    gb(msg,8,9)
+                print "Payload frames:\t", hex(frames)
+                print "Checksum:\t", hex(chk)
+                print "Our chk:\t", hex(getchk(msg[0:8]))
+                
+                payload = msg[9:9+(6*frames)]
+                parsepayload(payload)
                 
             if command == 0x0200:
                 print "DATA (ANSWER NEEDED)"
@@ -83,8 +92,51 @@ def parsebytes(data):
        
         print
         
+        
+def parsepayload(payload):
+    print ' '.join([hex(ord(i))[2:] for i in payload])
+    
+    data = []
+    
+    for i in range(len(payload)/6):
+        frame = payload[i*6:i*6+6]
+        print ' '.join([hex(ord(i))[2:] for i in frame])
+        
+        septet = ord(frame[4])
+        
+        for j in range(4):
+            if septet & (1 << j):
+                data.append(chr(ord(frame[j]) | 0x80))
+            else:
+                data.append(frame[j])
+                
+    print ' '.join([hex(ord(i))[2:] for i in data])
+    
+    temp1 = gb(data,0,2)
+    temp2 = gb(data,2,4)
+    temp3 = gb(data,4,6)
+    temp4 = gb(data,6,8)
+    pump1 = gb(data,8,9)
+    pump2 = gb(data,9,10)
+    
+    print "Temp1:\t", temp1, hex(temp1)
+    print "Temp2:\t", temp2, hex(temp2)
+    print "Temp3:\t", temp3, hex(temp3)
+    print "Temp4:\t", temp4, hex(temp4)
+    print "Pump1:\t", pump1, hex(pump1)
+    print "Pump2:\t", pump2, hex(pump2)
+    
+        
 def gb(data, begin, end): # GetBytes
     return sum([ord(b)<<(i*8) for i,b in enumerate(data[begin:end])])
+    
+def getchk(data):   
+    chk = 0x7F
+    for b in data:
+        chk = ((chk - ord(b)) % 0x100) & 0x7F
+        
+    return chk
+   
 
 
 
